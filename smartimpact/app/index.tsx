b8f1e63179473f77bc78c1361aa162e08b9c6cff
abant07 <abantwal2007@gmail.com>
2024-09-09 22:34:37 -0700

    title

--- smartimpact/app/index.tsx
+++ smartimpact/app/index.tsx
@@ -24,7 +24,7 @@ export default function HomeScreen() {
         return (
             <View style={styles.container}>
                 <Image style={styles.image} source={require('../assets/images/dimo.png')} />
-                <Text style={styles.text}>Instant Crash Detection</Text>
+                <Text style={styles.text}>ImpactIQ</Text>
                 <TouchableOpacity style={styles.connect} onPress={() => open()}>
                     <Text style={styles.connectText}>{isConnecting ? "Connecting..." : "Connect Wallet"}</Text>
                 </TouchableOpacity>
